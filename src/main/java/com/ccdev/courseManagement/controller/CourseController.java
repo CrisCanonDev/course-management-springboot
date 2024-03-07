b2007b794ce47245789b2a76958ecc12076a8c95
CrisCanonDev <criscanon21@gmail.com>
2024-03-07 23:31:53 +0200

    Adding methods at controller to addCourse and saveCourse via get and post mapping.

--- src/main/java/com/ccdev/courseManagement/controller/CourseController.java
+++ src/main/java/com/ccdev/courseManagement/controller/CourseController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -26,5 +29,27 @@ public class CourseController {
         List<Course> courses = courseRepository.findAll();
         model.addAttribute("courses", courses);
         return "courses";
+    }
+
+    @GetMapping("/courses/new")
+    public String addCourse(Model model){
+        List<String> semesters = Arrays.asList("Spring","Autumn");
+        Course course = new Course();
+        course.setPublished(true);
+        model.addAttribute("course", course);
+        model.addAttribute("pageTitle","New Course");
+        model.addAttribute("semesters", semesters);
+        return "course_form";
+    }
+
+    @PostMapping("/courses/save")
+    public String saveCourse(Course course, RedirectAttributes redirectAttributes){
+        try {
+            courseRepository.save(course);
+            redirectAttributes.addFlashAttribute("message", "the course has been saved successfully.");
+        }catch (Exception e){
+            redirectAttributes.addAttribute("message", e.getMessage());
+        }
+        return "redirect:/courses";
     }
 }
