626f7f13f37f9d86dc1413d90b0bee37741d7f81
CrisCanonDev <criscanon21@gmail.com>
2024-03-07 16:29:24 +0200

    Initial commit

---
+++ src/main/java/com/ccdev/courseManagement/CourseManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ccdev.courseManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementSpringbootApplication.class, args);
+	}
+
+}
