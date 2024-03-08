aeb871a52d23ffc85c1a2926f795d401a4aee401
CrisCanonDev <criscanon21@gmail.com>
2024-03-08 17:51:11 +0200

    Control class that generates PDF report.

---
+++ src/main/java/com/ccdev/courseManagement/controller/ExportsController.java
@@ -0,0 +1,37 @@
+package com.ccdev.courseManagement.controller;
+
+import com.ccdev.courseManagement.entity.Course;
+import com.ccdev.courseManagement.reports.ExportCoursePDF;
+import com.ccdev.courseManagement.repository.CourseRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class ExportsController {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @GetMapping("/export/pdf")
+    public void generateExportPDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentTimeFormat = dateFormat.format(new Date());
+
+        String headerKey ="Content-Disposition";
+        String headerValue = "attachment; filename=courses"+currentTimeFormat+".pdf";
+
+        List<Course> courses = courseRepository.findAll();
+
+        ExportCoursePDF exportCoursePDF = new ExportCoursePDF(courses);
+        exportCoursePDF.export(response);
+    }
+}
