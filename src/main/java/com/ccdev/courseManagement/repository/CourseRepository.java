c114c610befb72ebde89dd986d56f535d41bc9d8
CrisCanonDev <criscanon21@gmail.com>
2024-03-10 22:43:36 +0200

    Updating controller with parameters to enable pagination

--- src/main/java/com/ccdev/courseManagement/repository/CourseRepository.java
+++ src/main/java/com/ccdev/courseManagement/repository/CourseRepository.java
@@ -1,9 +1,12 @@
 package com.ccdev.courseManagement.repository;
 
 import com.ccdev.courseManagement.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+    Page<Course> fingTitleIgnoringCase(String keyword, Pageable pageable);
 }
